a6b2d1fecc4965733d5c680fd77e354672c34903
Andrey Kloninger <veto.aion@gmail.com>
2021-01-15 21:40:46 +0500

    адаптив preloader, resultnotfound, about 768px

--- news-frontend/src/components/Preloader/Preloader.js
+++ news-frontend/src/components/Preloader/Preloader.js
@@ -4,6 +4,7 @@ function Preloader() {
   return (
     <section className="preloader">
       <i className="preloader__circle"></i>
+      <p className="preloader__caption">Идёт поиск новостей...</p>
     </section>
   );
 }
