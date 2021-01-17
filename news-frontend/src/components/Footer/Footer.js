cd58dcc18eb1b42348ac7df85dea7b93e3c99a6f
Andrey Kloninger <veto.aion@gmail.com>
2021-01-17 13:22:22 +0500

    адаптив для Main, Navigation, Footer 320px

--- news-frontend/src/components/Footer/Footer.js
+++ news-frontend/src/components/Footer/Footer.js
@@ -8,18 +8,23 @@ function Footer() {
     <div className="footer">
       <p className="footer__copyright">&copy; 2020 Supresite, Powered by News API</p>
       <nav className="footer__navigation">
-        <Link className="footer__link" to="/">
-          Главная
-        </Link>
-        <a href="https://yandex.ru" className="footer__link">
-          Яндекс.Практикум
-        </a>
-        <a href="https://github.com" className="footer__link">
-          <img className="footer__link-icon" alt="Иконка Гитхаба" src={githubIcon} />
-        </a>
-        <a href="https://facebook.com" className="footer__link">
-          <img className="footer__link-icon" alt="Иконка Гитхаба" src={faceBookIcon} />
-        </a>
+        <div className="footer__navigation-column-link">
+          <Link className="footer__link" to="/">
+            Главная
+          </Link>
+          <a href="https://yandex.ru" className="footer__link">
+            Яндекс.Практикум
+          </a>
+        </div>
+
+        <div className="footer__social-link">
+          <a href="https://github.com" className="footer__link">
+            <img className="footer__link-icon" alt="Иконка Гитхаба" src={githubIcon} />
+          </a>
+          <a href="https://facebook.com" className="footer__link">
+            <img className="footer__link-icon" alt="Иконка Гитхаба" src={faceBookIcon} />
+          </a>
+        </div>
       </nav>
     </div>
   );
